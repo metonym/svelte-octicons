045134dd4228566349315854555d2e709a1e3f4c
Eric Liu <ericyl.us@gmail.com>
2020-06-10 18:40:51 -0700

    chore(deps-dev): upgrade @primer/octicons to v10.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [10.0.0](https://github.com/metonym/svelte-octicons/releases/tag/v10.0.0) - 2020-06-10
+
+- Upgrade @primer/octicons to version 10.0.0 (192 new icons)
+
 ## [9.6.3](https://github.com/metonym/svelte-octicons/releases/tag/v9.6.3) - 2020-05-11
 
 - Add `viewBox` attribute, export width, height props
--- build.js
+++ build.js
@@ -3,19 +3,19 @@ const octicons = require("@primer/octicons");
 const pkg = require("./package.json");
 
 const template = (octicon) => {
-  return `<script>export let width = ${
-    octicon.options.width
-  }; export let height = ${octicon.options.height};</script><svg
-  version="${octicon.options.version}"
+  return `<script>export let width = ${octicon.width}; export let height = ${
+    octicon.width
+  };</script><svg
   aria-hidden="${octicon.options["aria-hidden"]}"
+  version="${octicon.options.version}"
   viewBox="${octicon.options.viewBox}"
   {...$$restProps}
   {width}
   {height}
-${octicon.options.class
-  .split(" ")
-  .map((name) => `  class:${name}={true}`)
-  .join("\n")}
+  ${octicon.options.class
+    .split(" ")
+    .map((name) => `  class:${name}={true}`)
+    .join("\n")}
   on:click
   on:mouseover
   on:mouseenter
@@ -30,28 +30,33 @@ function build() {
   fs.mkdirSync("lib");
 
   const moduleNames = [];
+  const imports = [];
 
-  const imports = Object.keys(octicons).map((octicon) => {
-    const moduleName = octicon
-      .split("-")
-      .map((_) => _.slice(0, 1).toUpperCase() + _.slice(1))
-      .join("");
+  Object.keys(octicons).map((octicon) => {
+    Object.keys(octicons[octicon].heights).forEach((height) => {
+      const octiconByHeight = octicons[octicon].heights[height];
+      const moduleName =
+        octicon
+          .split("-")
+          .map((_) => _.slice(0, 1).toUpperCase() + _.slice(1))
+          .join("") + height;
 
-    moduleNames.push(moduleName);
+      moduleNames.push(moduleName);
 
-    fs.mkdirSync(`lib/${moduleName}`);
-    fs.writeFileSync(
-      `lib/${moduleName}/${moduleName}.svelte`,
-      template(octicons[octicon])
-    );
-    fs.writeFileSync(
-      `lib/${moduleName}/index.js`,
-      `import ${moduleName} from "./${moduleName}.svelte";
-       export { ${moduleName} };
-       export default ${moduleName};`
-    );
+      fs.mkdirSync(`lib/${moduleName}`);
+      fs.writeFileSync(
+        `lib/${moduleName}/${moduleName}.svelte`,
+        template(octiconByHeight)
+      );
+      fs.writeFileSync(
+        `lib/${moduleName}/index.js`,
+        `import ${moduleName} from "./${moduleName}.svelte";
+         export { ${moduleName} };
+         export default ${moduleName};`
+      );
 
-    return `export { ${moduleName} } from "./${moduleName}";`;
+      imports.push(`export { ${moduleName} } from "./${moduleName}";`);
+    });
   });
 
   fs.writeFileSync("lib/index.js", imports.join(""));

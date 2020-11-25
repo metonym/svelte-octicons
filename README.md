2a97c44225f76e05e013c74ec015b81c2470dd48
Eric Liu <ericyl.us@gmail.com>
2020-11-25 06:21:36 -0800

    chore: upgrade svelte-readme [ci skip]

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This library builds [GitHub Primer Octicons](https://primer.style/octicons/) as 
 
 Try it in the [Svelte REPL](https://svelte.dev/repl/dce762f9a93c4e56b3ddde749cb1945f?version=3.20.1).
 
-## [Icon Preview](https://metonym.github.io/svelte-octicons/) · [Icon Index](./ICON_INDEX.md)
+## [Icon Index](ICON_INDEX.md)
 
 ## Install
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import svelteReadme from "svelte-readme";
-import serve from "rollup-plugin-serve";
-
-const DEV = process.env.ROLLUP_WATCH;
 
 export default svelteReadme({
-  minify: !DEV,
+  minify: !process.env.ROLLUP_WATCH,
   svelte: { immutable: true },
   prefixUrl: "https://github.com/metonym/svelte-octicons/tree/master/",
-  plugins: [DEV && serve({ contentBase: "public", port: 3000 })],
+  style: `
+    .code-fence > div {
+      display: inline-flex;
+      flex-direction: column;
+      border: 1px solid #eaecef;
+      border-radius: 4px;
+      padding: 8px;
+      margin: 4px;
+    }
+
+    .code-fence svg {
+      margin-bottom: 4px;
+    }
+  `,
 });

60f95c98639897a2aa164b3ef7caf8da8994e8b0
Eric Liu <ericyl.us@gmail.com>
2021-05-27 09:58:23 -0700

    chore(deps-dev): upgrade @primer/octicons to v14.2 (#19)

--- test/Octicons.test.svelte
+++ test/Octicons.test.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-  import { Alert16, Rows16, Rocket16, Duplicate16 } from "../types";
+  import {
+    Alert16,
+    Rows16,
+    Rocket16,
+    Duplicate16,
+    KeyAsterisk16,
+  } from "../types";
   import Version24 from "../types/Verified24";
   import GitPullRequestClosed16 from "../types/GitPullRequestClosed16";
   import * as Octicons from "../types";
@@ -11,6 +17,7 @@
 <Version24 />
 <Duplicate16 />
 <GitPullRequestClosed16 height="20" />
+<KeyAsterisk16 />
 
 {#each Object.keys(Octicons) as octicon}
   <div>

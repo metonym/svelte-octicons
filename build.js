5df363d31ca3e9c46f34b23e87069442e9a6e8f3
metonym <ericyl.us@gmail.com>
2021-11-23 09:39:51 -0800

    build: remove await

--- build.js
+++ build.js
@@ -1,13 +1,11 @@
 const octicons = require("@primer/octicons");
-const svelvg = require("svelvg");
+const { createLibrary } = require("svelvg");
 
-(async () => {
-  await svelvg.createLibrary("@primer/octicons/build/svg", {
-    iconIndex: true,
-    appendClassNames: (filename) => {
-      const key = filename.slice(0, -3);
-      if (key in octicons) return ['octicon', `octicon-${key}`];
-      return [];
-    }
-  });
-})();
+createLibrary("@primer/octicons/build/svg", {
+  iconIndex: true,
+  appendClassNames: (filename) => {
+    const key = filename.slice(0, -3);
+    if (key in octicons) return ["octicon", `octicon-${key}`];
+    return [];
+  },
+});

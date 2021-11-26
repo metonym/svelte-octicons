8516a62d414b5e4a2d044810e9635baf91419a03
Eric Liu <ericyl.us@gmail.com>
2021-11-26 15:47:31 -0800

    refactor: use ESM instead of CJS

--- build.js
+++ build.js
@@ -1,11 +1,10 @@
-const octicons = require("@primer/octicons");
-const { createLibrary } = require("svelvg");
+import octicons from "@primer/octicons";
+import { createLibrary } from "svelvg";
 
 createLibrary("@primer/octicons/build/svg", {
   iconIndex: true,
   appendClassNames: (filename) => {
     const key = filename.slice(0, -3);
     if (key in octicons) return ["octicon", `octicon-${key}`];
-    return [];
   },
 });
